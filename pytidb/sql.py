f080593d94fc6f4815431dcd78892459c4d5e07c
Mini256 <minianter@foxmail.com>
2025-03-26 22:43:53 +0800

    fix: fix select expression and bump version to 0.0.2.post5

--- pytidb/sql.py
+++ pytidb/sql.py
@@ -1,5 +1,5 @@
 # Basic SQL operations
-from sqlalchemy.sql import select as select
+from sqlmodel.sql.expression import select as select
 from sqlalchemy.sql import insert as insert
 from sqlalchemy.sql import update as update
 from sqlalchemy.sql import delete as delete
@@ -21,30 +21,31 @@ from sqlalchemy.sql import intersect as intersect
 from sqlalchemy.sql import except_ as except_
 
 # Expression operations
-from sqlalchemy.sql.expression import and_ as and_
-from sqlalchemy.sql.expression import or_ as or_
-from sqlalchemy.sql.expression import not_ as not_
-from sqlalchemy.sql.expression import between as between
-from sqlalchemy.sql.expression import case as case
-from sqlalchemy.sql.expression import cast as cast
-from sqlalchemy.sql.expression import collate as collate
-from sqlalchemy.sql.expression import distinct as distinct
-from sqlalchemy.sql.expression import extract as extract
-from sqlalchemy.sql.expression import funcfilter as funcfilter
-from sqlalchemy.sql.expression import over as over
-from sqlalchemy.sql.expression import tuple_ as tuple_
-from sqlalchemy.sql.expression import type_coerce as type_coerce
-from sqlalchemy.sql.expression import within_group as within_group
+from sqlmodel.sql.expression import and_ as and_
+from sqlmodel.sql.expression import or_ as or_
+from sqlmodel.sql.expression import not_ as not_
+from sqlmodel.sql.expression import between as between
+from sqlmodel.sql.expression import case as case
+from sqlmodel.sql.expression import cast as cast
+from sqlmodel.sql.expression import col as col
+from sqlmodel.sql.expression import collate as collate
+from sqlmodel.sql.expression import distinct as distinct
+from sqlmodel.sql.expression import extract as extract
+from sqlmodel.sql.expression import funcfilter as funcfilter
+from sqlmodel.sql.expression import over as over
+from sqlmodel.sql.expression import tuple_ as tuple_
+from sqlmodel.sql.expression import type_coerce as type_coerce
+from sqlmodel.sql.expression import within_group as within_group
 
 # Aggregate functions
-from sqlalchemy.sql.expression import all_ as all_
-from sqlalchemy.sql.expression import any_ as any_
+from sqlmodel.sql.expression import all_ as all_
+from sqlmodel.sql.expression import any_ as any_
 
 # Ordering and null handling
-from sqlalchemy.sql.expression import asc as asc
-from sqlalchemy.sql.expression import desc as desc
-from sqlalchemy.sql.expression import nulls_first as nulls_first
-from sqlalchemy.sql.expression import nulls_last as nulls_last
+from sqlmodel.sql.expression import asc as asc
+from sqlmodel.sql.expression import desc as desc
+from sqlmodel.sql.expression import nulls_first as nulls_first
+from sqlmodel.sql.expression import nulls_last as nulls_last
 
 # Constants and special values
 from sqlalchemy.sql import null as null
@@ -84,6 +85,7 @@ __all__ = [
     "between",
     "case",
     "cast",
+    "col",
     "collate",
     "distinct",
     "extract",
