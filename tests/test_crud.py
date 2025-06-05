0dd82e075e59718d17a691d7fca8686825aa4ff1
Mini256 <minianter@foxmail.com>
2025-06-05 10:20:48 +0800

    refactor: reorganize filter tests into a new file

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -1,16 +1,11 @@
 import logging
-from typing import Any, Dict, List
-
+from typing import Any
 import numpy as np
-import pytest
 
-from pytidb.schema import TableModel, Field, VectorField, Column
-from pytidb.datatype import JSON
+from pytidb.schema import TableModel, Field, VectorField
 
 
 logger = logging.getLogger(__name__)
-
-# CRUD
 
 
 def test_table_crud(client):
@@ -57,75 +52,3 @@ def test_table_crud(client):
     assert columns[0].column_name == "id"
     assert columns[1].column_name == "text"
     assert columns[2].column_name == "text_vec"
-
-
-# Test filters
-
-
-@pytest.fixture(scope="module")
-def test_filters_table(client):
-    class ChunkWithMeta(TableModel, table=True):
-        __tablename__ = "test_filters_table"
-        id: int = Field(primary_key=True)
-        text: str = Field(max_length=20)
-        document_id: int = Field()
-        meta: Dict[str, Any] = Field(sa_column=Column(JSON))
-
-    tbl = client.create_table(schema=ChunkWithMeta, mode="overwrite")
-
-    test_data = [
-        ChunkWithMeta(
-            id=1,
-            text="foo",
-            document_id=1,
-            meta={"f": 0.2, "s": "apple", "a": [1, 2, 3]},
-        ),
-        ChunkWithMeta(
-            id=2,
-            text="bar",
-            document_id=2,
-            meta={"f": 0.5, "s": "banana", "a": [4, 5, 6]},
-        ),
-        ChunkWithMeta(
-            id=3,
-            text="biz",
-            document_id=2,
-            meta={"f": 0.7, "s": "cherry", "a": [7, 8, 9]},
-        ),
-    ]
-
-    tbl.bulk_insert(test_data)
-
-
-filter_test_data = [
-    pytest.param({"document_id": 1}, ["foo"], id="implicit $eq operator"),
-    pytest.param({"document_id": {"$eq": 1}}, ["foo"], id="explicit $eq operator"),
-    pytest.param({"id": {"$in": [2, 3]}}, ["bar", "biz"], id="$in operator"),
-    pytest.param({"id": {"$nin": [2, 3]}}, ["foo"], id="$nin operator"),
-    pytest.param({"id": {"$gte": 2}}, ["bar", "biz"], id="$gte operator"),
-    pytest.param({"id": {"$lt": 2}}, ["foo"], id="$lt operator"),
-    pytest.param(
-        {"$and": [{"document_id": 2}, {"id": {"$gt": 2}}]}, ["biz"], id="$and operator"
-    ),
-    pytest.param(
-        {"$or": [{"document_id": 1}, {"id": 3}]}, ["foo", "biz"], id="$or operator"
-    ),
-    pytest.param(
-        {"meta.f": {"$gte": 0.5}}, ["bar", "biz"], id="json column: $gt operator"
-    ),
-    pytest.param(
-        {"meta.s": {"$eq": "apple"}}, ["foo"], id="json column: explicit $eq operator"
-    ),
-    pytest.param({"meta.s": "apple"}, ["foo"], id="json column: implicit $eq operator"),
-]
-
-
-@pytest.mark.parametrize(
-    "filters,expected",
-    filter_test_data,
-)
-def test_filters(test_filters_table, filters: Dict[str, Any], expected: List[str]):
-    result = test_filters_table.query(filters)
-
-    actual = [r.text for r in result]
-    assert actual == expected
