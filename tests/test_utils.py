40afe214ebbef4b672c7bb014324f9f35a52b68a
ianthereal <argregoryian@gmail.com>
2025-10-21 15:36:26 +0000

    enable ssl refinement

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -27,6 +27,56 @@ def test_build_tidb_conn_url():
     assert url == "mysql+pymysql://root@localhost:4000/test"
 
 
+def test_build_tidb_conn_url_with_ca_path():
+    # For TiDB Serverless with custom CA path
+    url = build_tidb_connection_url(
+        host="gateway01.us-west-2.prod.aws.tidbcloud.com",
+        username="xxxxxxxx.root",
+        password="$password$",
+        ssl_ca_path="/path/to/ca-cert.pem",
+    )
+    assert (
+        url
+        == "mysql+pymysql://xxxxxxxx.root:%24password%24@gateway01.us-west-2.prod.aws.tidbcloud.com:4000/test?ssl_verify_cert=true&ssl_verify_identity=true&ssl_ca=%2Fpath%2Fto%2Fca-cert.pem"
+    )
+
+    # For local TiDB with explicit SSL and CA path
+    url = build_tidb_connection_url(
+        host="localhost",
+        username="root",
+        password="password",
+        enable_ssl=True,
+        ssl_ca_path="/custom/ca.pem",
+    )
+    assert (
+        url
+        == "mysql+pymysql://root:password@localhost:4000/test?ssl_verify_cert=true&ssl_verify_identity=true&ssl_ca=%2Fcustom%2Fca.pem"
+    )
+
+    # For dedicated TiDB cluster with ssl_ca_path only (P0 fix)
+    # This is the critical case: dedicated cluster (non-serverless hostname)
+    # with ssl_ca_path but no explicit enable_ssl - should auto-enable SSL
+    url = build_tidb_connection_url(
+        host="tidb-dedicated.xyz.prod.aws.tidbcloud.com",
+        username="root",
+        password="password",
+        ssl_ca_path="/path/to/ca.pem",
+    )
+    assert "ssl_verify_cert=true" in url
+    assert "ssl_verify_identity=true" in url
+    assert "ssl_ca=%2Fpath%2Fto%2Fca.pem" in url
+
+    # CA path with explicit disable_ssl should not include SSL params
+    url = build_tidb_connection_url(
+        host="localhost",
+        username="root",
+        password="password",
+        enable_ssl=False,
+        ssl_ca_path="/path/to/ca.pem",
+    )
+    assert url == "mysql+pymysql://root:password@localhost:4000/test"
+
+
 def test_build_tidb_conn_url_invalid():
     # Unacceptable schema
     with pytest.raises(ValueError):
